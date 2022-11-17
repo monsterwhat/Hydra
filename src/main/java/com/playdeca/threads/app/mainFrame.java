59fd0dd3279eb4e05afedd08bff552794c7376ef
Alvaro Padilla <monster_what@hotmail.com>
2022-11-16 22:22:19 -0800

    Update Visual

--- src/main/java/com/playdeca/threads/app/mainFrame.java
+++ src/main/java/com/playdeca/threads/app/mainFrame.java
@@ -27,10 +27,73 @@ public class mainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jComboBox1 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        btn_Start = new javax.swing.JButton();
+        jSlider1 = new javax.swing.JSlider();
+        jSlider2 = new javax.swing.JSlider();
+        jSlider3 = new javax.swing.JSlider();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        lblRound = new javax.swing.JLabel();
+        lblRound1 = new javax.swing.JLabel();
+        lblRound1_start = new javax.swing.JLabel();
+        lblRound1_end = new javax.swing.JLabel();
+        lblRound2 = new javax.swing.JLabel();
+        lblRound2_start = new javax.swing.JLabel();
+        lblRound2_end = new javax.swing.JLabel();
+        lblRound3 = new javax.swing.JLabel();
+        lblRound3_start = new javax.swing.JLabel();
+        lblRound3_end = new javax.swing.JLabel();
+        lblRound4 = new javax.swing.JLabel();
+        lblRound4_start = new javax.swing.JLabel();
+        lblRound4_end = new javax.swing.JLabel();
+        lblRound5 = new javax.swing.JLabel();
+        lblRound5_start = new javax.swing.JLabel();
+        lblRound5_end = new javax.swing.JLabel();
+        lblRound6 = new javax.swing.JLabel();
+        lblRound6_start = new javax.swing.JLabel();
+        lblRound6_end = new javax.swing.JLabel();
+        lblRound7 = new javax.swing.JLabel();
+        lblRound7_start = new javax.swing.JLabel();
+        lblRound7_end = new javax.swing.JLabel();
+        lblRound8 = new javax.swing.JLabel();
+        lblRound8_start = new javax.swing.JLabel();
+        lblRound8_end = new javax.swing.JLabel();
+        lblRound9 = new javax.swing.JLabel();
+        lblRound9_start = new javax.swing.JLabel();
+        lblRound9_end = new javax.swing.JLabel();
+        lblRound10 = new javax.swing.JLabel();
+        lblRound10_start = new javax.swing.JLabel();
+        lblRound10_end = new javax.swing.JLabel();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jProgressBar2 = new javax.swing.JProgressBar();
+        jProgressBar3 = new javax.swing.JProgressBar();
+        jProgressBar4 = new javax.swing.JProgressBar();
+        jProgressBar5 = new javax.swing.JProgressBar();
+        jProgressBar6 = new javax.swing.JProgressBar();
+        jProgressBar7 = new javax.swing.JProgressBar();
+        jProgressBar8 = new javax.swing.JProgressBar();
+        jProgressBar9 = new javax.swing.JProgressBar();
+        jProgressBar10 = new javax.swing.JProgressBar();
+        imgRound10 = new javax.swing.JLabel();
+        imgRound1 = new javax.swing.JLabel();
+        imgRound2 = new javax.swing.JLabel();
+        imgRound3 = new javax.swing.JLabel();
+        imgRound4 = new javax.swing.JLabel();
+        imgRound5 = new javax.swing.JLabel();
+        imgRound6 = new javax.swing.JLabel();
+        imgRound7 = new javax.swing.JLabel();
+        imgRound8 = new javax.swing.JLabel();
+        imgRound9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        setMinimumSize(new java.awt.Dimension(401, 600));
+        setResizable(false);
+        getContentPane().setLayout(null);
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         jComboBox1.addActionListener(new java.awt.event.ActionListener() {
@@ -38,15 +101,268 @@ public class mainFrame extends javax.swing.JFrame {
                 jComboBox1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 320, 146, 36));
+        getContentPane().add(jComboBox1);
+        jComboBox1.setBounds(30, 20, 270, 40);
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn_Start.setText("GO");
+        btn_Start.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn_StartActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 330, -1, -1));
+        getContentPane().add(btn_Start);
+        btn_Start.setBounds(320, 20, 50, 40);
+
+        jSlider1.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSlider1StateChanged(evt);
+            }
+        });
+        getContentPane().add(jSlider1);
+        jSlider1.setBounds(90, 80, 220, 20);
+        getContentPane().add(jSlider2);
+        jSlider2.setBounds(90, 110, 220, 20);
+        getContentPane().add(jSlider3);
+        jSlider3.setBounds(90, 140, 220, 20);
+
+        jLabel4.setText("jLabel4");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(30, 80, 37, 16);
+
+        jLabel5.setText("jLabel5");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(30, 110, 37, 16);
+
+        jLabel6.setText("jLabel6");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(30, 140, 37, 16);
+
+        jTextField1.setText("jTextField1");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(320, 80, 50, 22);
+
+        jTextField3.setText("jTextField3");
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(320, 110, 50, 22);
+
+        jTextField4.setText("jTextField4");
+        getContentPane().add(jTextField4);
+        jTextField4.setBounds(320, 140, 50, 22);
+        getContentPane().add(jSeparator1);
+        jSeparator1.setBounds(0, 180, 400, 10);
+
+        lblRound.setText("Round");
+        getContentPane().add(lblRound);
+        lblRound.setBounds(30, 200, 35, 16);
+
+        lblRound1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound1.setText("1");
+        getContentPane().add(lblRound1);
+        lblRound1.setBounds(30, 240, 30, 16);
+
+        lblRound1_start.setText("Inicio");
+        getContentPane().add(lblRound1_start);
+        lblRound1_start.setBounds(110, 240, 30, 16);
+
+        lblRound1_end.setText("Fin");
+        getContentPane().add(lblRound1_end);
+        lblRound1_end.setBounds(360, 240, 20, 16);
+
+        lblRound2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound2.setText("2");
+        getContentPane().add(lblRound2);
+        lblRound2.setBounds(30, 270, 30, 16);
+
+        lblRound2_start.setText("Inicio");
+        getContentPane().add(lblRound2_start);
+        lblRound2_start.setBounds(110, 270, 29, 16);
+
+        lblRound2_end.setText("Fin");
+        getContentPane().add(lblRound2_end);
+        lblRound2_end.setBounds(360, 270, 20, 16);
+
+        lblRound3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound3.setText("3");
+        getContentPane().add(lblRound3);
+        lblRound3.setBounds(30, 300, 30, 16);
+
+        lblRound3_start.setText("Inicio");
+        getContentPane().add(lblRound3_start);
+        lblRound3_start.setBounds(110, 300, 29, 16);
+
+        lblRound3_end.setText("Fin");
+        getContentPane().add(lblRound3_end);
+        lblRound3_end.setBounds(360, 300, 16, 16);
+
+        lblRound4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound4.setText("4");
+        getContentPane().add(lblRound4);
+        lblRound4.setBounds(30, 330, 30, 16);
+
+        lblRound4_start.setText("Inicio");
+        getContentPane().add(lblRound4_start);
+        lblRound4_start.setBounds(110, 330, 30, 16);
+
+        lblRound4_end.setText("Fin");
+        getContentPane().add(lblRound4_end);
+        lblRound4_end.setBounds(360, 330, 20, 16);
+
+        lblRound5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound5.setText("5");
+        getContentPane().add(lblRound5);
+        lblRound5.setBounds(30, 360, 30, 16);
+
+        lblRound5_start.setText("Inicio");
+        getContentPane().add(lblRound5_start);
+        lblRound5_start.setBounds(110, 360, 29, 16);
+
+        lblRound5_end.setText("Fin");
+        getContentPane().add(lblRound5_end);
+        lblRound5_end.setBounds(360, 360, 16, 16);
+
+        lblRound6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound6.setText("6");
+        getContentPane().add(lblRound6);
+        lblRound6.setBounds(30, 390, 30, 16);
+
+        lblRound6_start.setText("Inicio");
+        getContentPane().add(lblRound6_start);
+        lblRound6_start.setBounds(110, 390, 29, 16);
+
+        lblRound6_end.setText("Fin");
+        getContentPane().add(lblRound6_end);
+        lblRound6_end.setBounds(360, 390, 16, 16);
+
+        lblRound7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound7.setText("7");
+        getContentPane().add(lblRound7);
+        lblRound7.setBounds(30, 420, 30, 16);
+
+        lblRound7_start.setText("Inicio");
+        getContentPane().add(lblRound7_start);
+        lblRound7_start.setBounds(110, 420, 29, 16);
+
+        lblRound7_end.setText("Fin");
+        getContentPane().add(lblRound7_end);
+        lblRound7_end.setBounds(360, 420, 16, 16);
+
+        lblRound8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound8.setText("8");
+        getContentPane().add(lblRound8);
+        lblRound8.setBounds(30, 450, 30, 16);
+
+        lblRound8_start.setText("Inicio");
+        getContentPane().add(lblRound8_start);
+        lblRound8_start.setBounds(110, 450, 30, 16);
+
+        lblRound8_end.setText("Fin");
+        getContentPane().add(lblRound8_end);
+        lblRound8_end.setBounds(360, 450, 20, 16);
+
+        lblRound9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound9.setText("9");
+        getContentPane().add(lblRound9);
+        lblRound9.setBounds(30, 480, 30, 16);
+
+        lblRound9_start.setText("Inicio");
+        getContentPane().add(lblRound9_start);
+        lblRound9_start.setBounds(110, 480, 29, 16);
+
+        lblRound9_end.setText("Fin");
+        getContentPane().add(lblRound9_end);
+        lblRound9_end.setBounds(360, 480, 16, 16);
+
+        lblRound10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRound10.setText("10");
+        getContentPane().add(lblRound10);
+        lblRound10.setBounds(30, 510, 30, 16);
+
+        lblRound10_start.setText("Inicio");
+        getContentPane().add(lblRound10_start);
+        lblRound10_start.setBounds(110, 510, 29, 16);
+
+        lblRound10_end.setText("Fin");
+        getContentPane().add(lblRound10_end);
+        lblRound10_end.setBounds(360, 510, 16, 16);
+        getContentPane().add(jProgressBar1);
+        jProgressBar1.setBounds(150, 240, 200, 20);
+        getContentPane().add(jProgressBar2);
+        jProgressBar2.setBounds(150, 270, 200, 20);
+        getContentPane().add(jProgressBar3);
+        jProgressBar3.setBounds(150, 300, 200, 20);
+        getContentPane().add(jProgressBar4);
+        jProgressBar4.setBounds(150, 330, 200, 20);
+        getContentPane().add(jProgressBar5);
+        jProgressBar5.setBounds(150, 360, 200, 20);
+        getContentPane().add(jProgressBar6);
+        jProgressBar6.setBounds(150, 390, 200, 20);
+        getContentPane().add(jProgressBar7);
+        jProgressBar7.setBounds(150, 420, 200, 20);
+        getContentPane().add(jProgressBar8);
+        jProgressBar8.setBounds(150, 450, 200, 20);
+        getContentPane().add(jProgressBar9);
+        jProgressBar9.setBounds(150, 480, 200, 20);
+        getContentPane().add(jProgressBar10);
+        jProgressBar10.setBounds(150, 510, 200, 20);
+
+        imgRound10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound10.setText("jLabel1");
+        getContentPane().add(imgRound10);
+        imgRound10.setBounds(70, 500, 30, 30);
+
+        imgRound1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/tenor-2700125230(1).gif"))); // NOI18N
+        getContentPane().add(imgRound1);
+        imgRound1.setBounds(70, 230, 30, 30);
+
+        imgRound2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound2.setText("jLabel1");
+        getContentPane().add(imgRound2);
+        imgRound2.setBounds(70, 260, 30, 30);
+
+        imgRound3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound3.setText("jLabel1");
+        getContentPane().add(imgRound3);
+        imgRound3.setBounds(70, 290, 30, 30);
+
+        imgRound4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound4.setText("jLabel1");
+        getContentPane().add(imgRound4);
+        imgRound4.setBounds(70, 320, 30, 30);
+
+        imgRound5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound5.setText("jLabel1");
+        getContentPane().add(imgRound5);
+        imgRound5.setBounds(70, 350, 30, 30);
+
+        imgRound6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound6.setText("jLabel1");
+        getContentPane().add(imgRound6);
+        imgRound6.setBounds(70, 380, 30, 30);
+
+        imgRound7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound7.setText("jLabel1");
+        getContentPane().add(imgRound7);
+        imgRound7.setBounds(70, 410, 30, 30);
+
+        imgRound8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound8.setText("jLabel1");
+        getContentPane().add(imgRound8);
+        imgRound8.setBounds(70, 440, 30, 30);
+
+        imgRound9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flag.png"))); // NOI18N
+        imgRound9.setText("jLabel1");
+        getContentPane().add(imgRound9);
+        imgRound9.setBounds(70, 470, 30, 30);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -55,9 +371,23 @@ public class mainFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jComboBox1ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_StartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_StartActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btn_StartActionPerformed
+
+    private void jSlider1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSlider1StateChanged
+        // TODO add your handling code here:
+        
+        
+    }//GEN-LAST:event_jSlider1StateChanged
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -95,7 +425,68 @@ public class mainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btn_Start;
+    private javax.swing.JLabel imgRound1;
+    private javax.swing.JLabel imgRound10;
+    private javax.swing.JLabel imgRound2;
+    private javax.swing.JLabel imgRound3;
+    private javax.swing.JLabel imgRound4;
+    private javax.swing.JLabel imgRound5;
+    private javax.swing.JLabel imgRound6;
+    private javax.swing.JLabel imgRound7;
+    private javax.swing.JLabel imgRound8;
+    private javax.swing.JLabel imgRound9;
     private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JProgressBar jProgressBar10;
+    private javax.swing.JProgressBar jProgressBar2;
+    private javax.swing.JProgressBar jProgressBar3;
+    private javax.swing.JProgressBar jProgressBar4;
+    private javax.swing.JProgressBar jProgressBar5;
+    private javax.swing.JProgressBar jProgressBar6;
+    private javax.swing.JProgressBar jProgressBar7;
+    private javax.swing.JProgressBar jProgressBar8;
+    private javax.swing.JProgressBar jProgressBar9;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSlider jSlider1;
+    private javax.swing.JSlider jSlider2;
+    private javax.swing.JSlider jSlider3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JLabel lblRound;
+    private javax.swing.JLabel lblRound1;
+    private javax.swing.JLabel lblRound10;
+    private javax.swing.JLabel lblRound10_end;
+    private javax.swing.JLabel lblRound10_start;
+    private javax.swing.JLabel lblRound1_end;
+    private javax.swing.JLabel lblRound1_start;
+    private javax.swing.JLabel lblRound2;
+    private javax.swing.JLabel lblRound2_end;
+    private javax.swing.JLabel lblRound2_start;
+    private javax.swing.JLabel lblRound3;
+    private javax.swing.JLabel lblRound3_end;
+    private javax.swing.JLabel lblRound3_start;
+    private javax.swing.JLabel lblRound4;
+    private javax.swing.JLabel lblRound4_end;
+    private javax.swing.JLabel lblRound4_start;
+    private javax.swing.JLabel lblRound5;
+    private javax.swing.JLabel lblRound5_end;
+    private javax.swing.JLabel lblRound5_start;
+    private javax.swing.JLabel lblRound6;
+    private javax.swing.JLabel lblRound6_end;
+    private javax.swing.JLabel lblRound6_start;
+    private javax.swing.JLabel lblRound7;
+    private javax.swing.JLabel lblRound7_end;
+    private javax.swing.JLabel lblRound7_start;
+    private javax.swing.JLabel lblRound8;
+    private javax.swing.JLabel lblRound8_end;
+    private javax.swing.JLabel lblRound8_start;
+    private javax.swing.JLabel lblRound9;
+    private javax.swing.JLabel lblRound9_end;
+    private javax.swing.JLabel lblRound9_start;
     // End of variables declaration//GEN-END:variables
 }
